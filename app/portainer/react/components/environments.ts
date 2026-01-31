18e445ea029889b5218f53a5b6e7102485b0d7ad
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2026-01-31 15:05:11 +0700

    refactor(environments): migrate item view to react [BE-6632] (#1747)

--- app/portainer/react/components/environments.ts
+++ app/portainer/react/components/environments.ts
@@ -1,82 +1,10 @@
 import angular from 'angular';
 
 import { r2a } from '@/react-tools/react2angular';
-import { EdgeKeyDisplay } from '@/react/portainer/environments/ItemView/EdgeKeyDisplay';
-import { EdgeAgentDeploymentWidget } from '@/react/portainer/environments/ItemView/EdgeAgentDeploymentWidget/EdgeAgentDeploymentWidget';
 import { KVMControl } from '@/react/portainer/environments/KvmView/KVMControl';
 import { TagsDatatable } from '@/react/portainer/environments/TagsView/TagsDatatable';
-import { EnvironmentBasicConfigSection } from '@/react/portainer/environments/ItemView/EnvironmentBasicConfigSection/EnvironmentBasicConfigSection';
-import { EdgeInformationPanel } from '@/react/portainer/environments/ItemView/EdgeInformationPanel/EdgeInformationPanel';
-import { KubeConfigInfo } from '@/react/portainer/environments/ItemView/KubeConfigInfo/KubeConfigInfo';
-import { withReactQuery } from '@/react-tools/withReactQuery';
-import { withCurrentUser } from '@/react-tools/withCurrentUser';
-import { withUIRouter } from '@/react-tools/withUIRouter';
-import { AzureEnvironmentForm } from '@/react/portainer/environments/ItemView/AzureEnvironmentForm/AzureEnvironmentForm';
-import { GeneralEnvironmentForm } from '@/react/portainer/environments/ItemView/GeneralEnvironmentForm/GeneralEnvironmentForm';
-import { EdgeEnvironmentForm } from '@/react/portainer/environments/ItemView/EdgeEnvironmentForm/EdgeEnvironmentForm';
 
 export const environmentsModule = angular
   .module('portainer.app.react.components.environments', [])
-  .component('edgeKeyDisplay', r2a(EdgeKeyDisplay, ['edgeKey']))
-  .component(
-    'edgeAgentDeploymentWidget',
-    r2a(withCurrentUser(withUIRouter(EdgeAgentDeploymentWidget)), [
-      'edgeKey',
-      'edgeId',
-      'asyncMode',
-    ])
-  )
-  .component(
-    'kubeConfigInfo',
-    r2a(withUIRouter(withReactQuery(KubeConfigInfo)), [
-      'environmentId',
-      'environmentType',
-      'edgeId',
-      'status',
-    ])
-  )
   .component('kvmControl', r2a(KVMControl, ['deviceId', 'server', 'token']))
-  .component('tagsDatatable', r2a(TagsDatatable, ['dataset', 'onRemove']))
-  .component(
-    'environmentBasicConfigSection',
-    r2a(EnvironmentBasicConfigSection, [
-      'values',
-      'setValues',
-      'isEdge',
-      'isAzure',
-      'isAgent',
-      'hasError',
-      'isLocalEnvironment',
-    ])
-  )
-  .component(
-    'edgeInformationPanel',
-    r2a(withUIRouter(withReactQuery(EdgeInformationPanel)), [
-      'environmentId',
-      'edgeKey',
-      'edgeId',
-      'platformName',
-      'onSuccess',
-    ])
-  )
-  .component(
-    'azureEnvironmentForm',
-    r2a(withUIRouter(withCurrentUser(AzureEnvironmentForm)), [
-      'environment',
-      'onSuccess',
-    ])
-  )
-  .component(
-    'generalEnvironmentForm',
-    r2a(withUIRouter(withCurrentUser(GeneralEnvironmentForm)), [
-      'environment',
-      'onSuccess',
-    ])
-  )
-  .component(
-    'edgeEnvironmentForm',
-    r2a(withUIRouter(withCurrentUser(EdgeEnvironmentForm)), [
-      'environment',
-      'onSuccess',
-    ])
-  ).name;
+  .component('tagsDatatable', r2a(TagsDatatable, ['dataset', 'onRemove'])).name;
--- app/portainer/react/views/environments.ts
+++ app/portainer/react/views/environments.ts
@@ -5,12 +5,17 @@ import { withCurrentUser } from '@/react-tools/withCurrentUser';
 import { withUIRouter } from '@/react-tools/withUIRouter';
 import { ListView } from '@/react/portainer/environments/ListView';
 import { EdgeAutoCreateScriptViewWrapper } from '@/react/portainer/environments/EdgeAutoCreateScriptView/EdgeAutoCreateScriptView';
+import { ItemView } from '@/react/portainer/environments/ItemView/ItemView';
 
 export const environmentsModule = angular
   .module('portainer.app.react.views.environments', [])
   .component(
     'environmentsListView',
     r2a(withUIRouter(withCurrentUser(ListView)), [])
+  )
+  .component(
+    'environmentsItemView',
+    r2a(withUIRouter(withCurrentUser(ItemView)), [])
   )
   .component(
     'edgeAutoCreateScriptView',
